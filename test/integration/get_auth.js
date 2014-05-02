c55881b16d9418083dd78e3931136f65e26ab2b6
remixz <zbruggeman@me.com>
2014-05-01 17:01:59 -0700

    Browser testing
    * Make all tests work in browser
    * Add zuul for cross-browser testing with Selenium

--- test/integration/get_auth.js
+++ test/integration/get_auth.js
@@ -1,12 +1,12 @@
 var test    = require('tape'),
     diy     = require('../../lib/index')('*');
 
-diy({
-    method: 'GET',
-    uri:    '/explore/featured',
-    auth:   'username:password'
-}, function (err, body) {
-    test('HTTP GET w/ Basic Auth', function (t) {
+test('HTTP GET w/ Basic Auth', function (t) {
+    diy({
+        method: 'GET',
+        uri:    '/explore/featured',
+        auth:   'username:password'
+    }, function (err, body) {
         t.equal(err, null, 'errors should be null');
         t.equal(typeof body, 'object', 'response body should be an object');
         t.end();
--- test/integration/post_err.js
+++ test/integration/post_err.js
@@ -1,17 +1,26 @@
 var test    = require('tape'),
     diy     = require('../../lib/index')('*');
 
-diy({
-    method: 'POST',
-    uri:    '/loopback',
-    params: {
-        foo: 'bar'
-    }
-}, function (err, body) {
-    test('HTTP POST w/ error', function (t) {
-        t.equal(err, null, 'errors should be null');
-        t.equal(typeof body, 'object', 'response body should be an object');
-        t.equal(body.response.error, 'Method Not Allowed', 'included expected response');
-        t.end();
+test('HTTP POST w/ error', function (t) {
+    diy({
+        method: 'POST',
+        uri:    '/loopback',
+        params: {
+            foo: 'bar'
+        }
+    }, function (err, body) {
+        if (typeof window !== 'undefined') {
+            var body = JSON.parse(err.message);
+
+            t.notEqual(err, null, 'errors should not be null');
+            t.equal(typeof body, 'object', 'error is a valid object');
+            t.equal(body.response.error, 'Method Not Allowed', 'included expected response');
+            t.end();
+        } else {
+            t.equal(err, null, 'errors should be null');
+            t.equal(typeof body, 'object', 'response body should be an object');
+            t.equal(body.response.error, 'Method Not Allowed', 'included expected response');
+            t.end();
+        }
     });
 });
